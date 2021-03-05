f724bbd7b08b5c1dc15360c9a985421a93a5129d
Victor Nikliaiev <victor.nikliaiev@gmail.com>
2021-03-06 01:08:50 +0200

    modified loading functionality

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Loading = () => {
+  const [dots, setDots] = useState("");
+
+  useEffect(() => {
+    let dotCounter = 0;
+    if (Loading) {
+      const timer = setInterval(() => {
+        try {
+          setDots((prev) => prev + "|");
+          dotCounter += 1;
+          if (dotCounter === 20) {
+            setDots("");
+            dotCounter = 0;
+            clearInterval(timer);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }, 8);
+    }
+  }, []);
   return (
     <main className="container">
-      <h1>Loading...</h1>
+      <h1>{dots}</h1>
     </main>
   );
 };
