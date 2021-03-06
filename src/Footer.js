e29c19d9d513b029a5aa9d987ae889ffec5f077e
Victor Nikliaiev <victor.nikliaiev@gmail.com>
2021-03-06 12:06:21 +0200

    Fixed some design errors

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { GoEyeClosed } from "react-icons/go";
-import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = ({ closeFooter }) => {
   return (
     <footer className="footer">
-      <div class="c-btn btn-two" onClick={closeFooter}>
+      <div className="c-btn btn-two" onClick={closeFooter}>
         <span>
           <GoEyeClosed className="close-icon" />
         </span>
       </div>
       <h2>Welcome, friend!</h2>
-      <p>
-        This is an experimental project. Arhis Alight {<FaRegCopyright />} 2021
-      </p>
+      <p>This is an experimental project. Arhis Alight 2021</p>
     </footer>
   );
 };
